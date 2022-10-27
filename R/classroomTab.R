944cbd201b1ac9bbd75339cc5f68f8bff3c1e4fc
Echinoidea <gabriel.i.hooks@gmail.com>
2022-10-27 13:46:21 -0400

    Boilerplate

---
+++ R/classroomTab.R
@@ -0,0 +1,15 @@
+classroomTabUI <- function(id, uploadedData) {
+  ns <- NS(id)
+  
+  tabItem(
+    tabName = "classroomTab"
+    
+    
+  )
+}
+
+classroomTabServer <- function(id, uploadedData) {
+  moduleServer(id, function(input, output, session) {
+    
+  })
+}
