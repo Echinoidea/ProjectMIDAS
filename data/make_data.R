f1a23034f6aea42a0f5b0316d8bba00a3e67c8de
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2022-11-09 15:49:19 -0500

    Updated data. Removed teacher names, added teacher IDs and room IDs

--- data/make_data.R
+++ data/make_data.R
@@ -4,10 +4,15 @@ names <- read.delim("./data/Names.txt", sep = "\n", header = FALSE)
 
 # Add generated MIDAS risk score
 df$midasRisk <- sample(1:50, nrow(df), replace = TRUE)
-df$homeroomName <- rep(names[sample(1:20), ], 50)
+
+df$homeroomId <- rep(names[sample(1:20), ], 50)
+df$homeroomTeacherId <- sample(1:1000, nrow(df), replace = FALSE)
 df$homeroomScore <- sample(1:100, prob = 1:100, nrow(df), replace = TRUE)
-df$subjectRoomName <- rep(names[sample(11:30), ], 50)
+
+df$subjectRoomId <- rep(names[sample(11:30), ], 50)
+df$subjectROomTeacherId <- sample(1:1000, nrow(df), replace = FALSE)
 df$subjectRoomScore <- sample(1:100, prob = 1:100, nrow(df), replace = TRUE)
+
 df$selfReportScore <- sample(1:100, prob = 1:100, nrow(df), replace = TRUE)
 
 
